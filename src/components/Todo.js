7513a1285573b5ecfe18a25d0dea2da91f98c3f1
aalekh <aalekhpradeep@gmail.com>
2022-01-14 21:30:14 +0530

    Ref demo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { Component } from "react";
 
-const Todo=()=>{
-    return(
-        <>
+class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "aalekh",
+    };
+    this.inputRef = React.createRef();
+  }
+  componentDidMount() {
+    this.inputRef.current.focus();
+    console.log("MOunting", this.inputRef);
+  }
+
+  handleClick=()=>{
+      alert("Welcome "+this.inputRef.current.value);
+  }
+  render() {
+    return (
+      <>
         <div className="container-fluid">
-            <div className='row'>
-                <div className='col-12' style={{textAlign:"center"}}>
-                    <h1>HELLO WORLD</h1>
-                </div>
+          <div className="row">
+            <div className="col-12" style={{ textAlign: "center" }}>
+              <h1>HELLO WORLD</h1>
+              <input typeof="text" ref={this.inputRef} />
             </div>
+            <div className="col-12" style={{ textAlign: "center" }}>
+              <button className="btn btn-primary" onClick={this.handleClick}>Click Here</button>
+            </div>
+          </div>
         </div>
-        </>
-    )
+      </>
+    );
+  }
 }
 
 export default Todo;
