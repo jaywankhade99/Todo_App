0aa06c520625e1699516388fa4cf3539da2cf441
aalekh <aalekhpradeep@gmail.com>
2022-01-14 21:21:57 +0530

    First commit

---
+++ src/components/Todo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Todo=()=>{
+    return(
+        <>
+        <div className="container-fluid">
+            <div className='row'>
+                <div className='col-12' style={{textAlign:"center"}}>
+                    <h1>HELLO WORLD</h1>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Todo;
